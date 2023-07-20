3898a3e4b7454810d10f6232b6384c99c17d4f9b
Callum Alexander <callumalexander.personal@gmail.com>
2023-07-20 11:23:42 +0100

    Start of day - install ideavim for my horrible purposes and created a main file to test manually in CLI outside the flask app

---
+++ main.py
@@ -0,0 +1,11 @@
+from Market.core import Equity, Market
+from MarketMaker.core import MarketMaker
+
+if __name__ == "__main__":
+    stock = Equity(100, 0.2, "APPL")
+    market = Market(stock, 100)
+    market.simulate_market()
+
+    maker = MarketMaker(stock)
+    for order in market.orders:
+        print(maker.execute_order(order))
